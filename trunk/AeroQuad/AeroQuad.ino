d4bc01813101f7a1262cfe9c46efc6f5011475e1
s@iamsurya.com <s@iamsurya.com@3459e4e7-7145-f115-2401-f79c86914188>
2012-04-15 14:20:39 +0000

    Userconfiguration Cleared.

--- trunk/AeroQuad/AeroQuad.ino
+++ trunk/AeroQuad/AeroQuad.ino
@@ -1,31 +1,3 @@
-/*
-  AeroQuad v3.0 - December 2011
-  www.AeroQuad.com
-  Copyright (c) 2011 Ted Carancho.  All rights reserved.
-  An Open Source Arduino based multicopter.
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/****************************************************************************
-   Before flight, select the different user options for your AeroQuad by
-   editing UserConfiguration.h.
-
-   If you need additional assitance go to http://www.aeroquad.com/forum.php
-   or talk to us live on IRC #aeroquad
-*****************************************************************************/
-
 #include "UserConfiguration.h" // Edit this file first before uploading to the AeroQuad
 
 // Checks to make sure we have the right combinations defined
