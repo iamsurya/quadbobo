584d83341c7cb09e08903cad4c37b3a4a95ab875
s@iamsurya.com <s@iamsurya.com@3459e4e7-7145-f115-2401-f79c86914188>
2012-04-15 14:24:18 +0000

    UserConfiguration.h moved to main AQ code and deleted.

--- AeroQuad/UserConfiguration.h
+++
@@ -1,5 +0,0 @@
-#define AeroQuad_v18        // Arduino 2009 with AeroQuad Shield v1.8 or greater
-#define quadPlusConfig
-#define RateModeOnly // Use this if you only have a gyro sensor, this will disable any attitude modes.
-#define LASTCHANNEL 6
-
